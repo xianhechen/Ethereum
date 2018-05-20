ae50c81cdfddfaabf5af176febe8d8485ce91c91
xianhechen <chenxi28@msu.edu>
2018-05-19 21:15:14 -0700

    Solved several compatibility issues

--- Lottery/Contracts/Lottery.sol
+++ Lottery/Contracts/Lottery.sol
@@ -1,9 +1,20 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.24;
 
 contract Lottery {
     address public manager;
+    address[] public players;
 
-    constructor () public {
+    constructor() public payable {
         manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+
+        players.push(msg.sender);
+    }
+    
+    function random() public view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, now, players)));
     }
 }
