00781158d1cc30e1fafbdaa9cb30f930a6430bea
xianhechen <chenxi28@msu.edu>
2018-05-20 17:03:42 -0700

    initialized tests for Lottery project

--- Lottery/Contracts/Lottery.sol
+++ Lottery/Contracts/Lottery.sol
@@ -13,8 +13,25 @@ contract Lottery {
 
         players.push(msg.sender);
     }
-    
+
     function random() public view returns (uint) {
         return uint(keccak256(abi.encodePacked(block.difficulty, now, players)));
+    }
+
+    function pickWinner() public  restricted {
+        uint index = random() % players.length;
+        players[index].transfer(address(this).balance);
+        // reset contract
+        players = new address[](0); // initial size of 0
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    // function for web apps to get players
+    function getPlayers() public view returns (address[]) {
+        return players;
     }
 }
---
+++ Lottery/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider ( // infura provider
+  'father reunion logic athlete fiction clever helmet enemy fix curve rail funny',
+  'https://rinkeby.infura.io/oAkWNrCw1R8oI3v0W6kG' // easier than geth? lol
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attemppting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: '0x'+bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to: ', result.options.address);
+};
+deploy();
