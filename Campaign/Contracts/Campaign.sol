7f6526ca54b23322f5f7e88f3cfdec8adcc85b02
xianhechen <chenxi28@msu.edu>
2018-05-24 21:07:28 -0700

    more on contract

---
+++ Campaign/Contracts/Campaign.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.17;
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    constructor (uint minimum) public {
+        manager = msg.sender;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+
+        approvers[msg.sender] = true;
+    }
+
+    function createRequest(string description, uint value, address recipient) public restricted {
+
+        Request memory newRequest = Request({
+           description: description,
+           value: value,
+           recipient: recipient,
+           complete: false,
+           approvalCount: 0
+        });
+
+        requests.push(newRequest);
+    }
+
+}
