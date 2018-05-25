53c49172a5d11ed55b3b31c1f22273957a52cb5f
xianhechen <chenxi28@msu.edu>
2018-05-24 22:20:59 -0700

    added factory to get better scalability and security

--- Campaign/Contracts/Campaign.sol
+++ Campaign/Contracts/Campaign.sol
@@ -1,5 +1,18 @@
 pragma solidity ^0.4.17;
 
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
+
 contract Campaign {
     struct Request {
         string description;
@@ -21,8 +34,8 @@ contract Campaign {
         _;
     }
 
-    constructor (uint minimum) public {
-        manager = msg.sender;
+    constructor (uint minimum, address creator) public {
+        manager = creator;
         minimumContribution = minimum;
     }
 
